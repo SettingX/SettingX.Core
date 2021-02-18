68318a874502cc345a5892780229de59648c0235
Nika Gamkrelidze <nikagamkrelidze233@gmail.com>
2021-02-18 15:45:37 +0400

    KeyValues repo fixed.

--- SettingX.Core/Repositories/IKeyValuesRepository.cs
+++ SettingX.Core/Repositories/IKeyValuesRepository.cs
@@ -9,13 +9,13 @@ namespace SettingX.Core.Repositories
     {
         Task<Dictionary<string, KeyValue>> GetAsync();
         Task<KeyValue> GetTopRecordAsync();
-        Task<IEnumerable<KeyValue>> GetAsync(Func<KeyValue, bool> filter);
-        Task<IEnumerable<KeyValue>> GetKeyValuesAsync();
-        Task<IEnumerable<KeyValue>> GetKeyValuesAsync(Func<KeyValue, bool> filter, string repositoryId = null);
+        Task<List<KeyValue>> GetAsync(Func<KeyValue, bool> filter);
+        Task<List<KeyValue>> GetKeyValuesAsync();
+        Task<List<KeyValue>> GetKeyValuesAsync(Func<KeyValue, bool> filter, string repositoryId = null);
         Task<KeyValue> GetKeyValueAsync(string key);
         Task<Dictionary<string, KeyValue>> GetKeyValuesAsync(IEnumerable<string> keys);
-        Task<bool> UpdateKeyValueAsync(IEnumerable<KeyValue> keyValueList);
-        Task<bool> ReplaceKeyValueAsync(IEnumerable<KeyValue> keyValueList);
+        Task<bool> UpdateKeyValueAsync(List<KeyValue> keyValueList);
+        Task<bool> ReplaceKeyValueAsync(List<KeyValue> keyValueList);
         Task RemoveNetworkOverridesAsync(string networkId);
         Task DeleteKeyValueWithHistoryAsync(
             string keyValueId,
