2a507e77b92e4e47c66ca3d399ae76d864aa9edc
Nika Gamkrelidze <nikagamkrelidze233@gmail.com>
2021-02-19 10:02:15 +0400

    more fixe.

--- SettingX.Core/Repositories/IRepositoriesRepository.cs
+++ SettingX.Core/Repositories/IRepositoriesRepository.cs
@@ -8,9 +8,8 @@ namespace SettingX.Core.Repositories
     public interface IRepositoriesRepository
     {
         Task<Repository> GetAsync(string repositoryId);
-        Task<IEnumerable<Repository>> GetAllAsync();
+        Task<List<Repository>> GetAllAsync();
         Task RemoveRepositoryAsync(string repositoryId);
         Task SaveRepositoryAsync(Repository repository);
-        Task<IEnumerable<Repository>> GetAsync(Func<Repository, bool> filter);
     }
 }
