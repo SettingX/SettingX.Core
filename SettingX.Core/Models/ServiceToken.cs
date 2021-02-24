dd5532a7f25f6bf5ef035dd95e28b52493d148cf
Nika Gamkrelidze <nikagamkrelidze233@gmail.com>
2021-02-24 07:01:56 +0400

    ServiceToken json property names added.

--- SettingX.Core/Models/ServiceToken.cs
+++ SettingX.Core/Models/ServiceToken.cs
@@ -1,9 +1,16 @@
+using System.Text.Json.Serialization;
+
 namespace SettingX.Core.Models
 {
     public class ServiceToken
     {
+        [JsonPropertyName("token")]
         public string Token { get; set; }
+        
+        [JsonPropertyName("security_key_one")]
         public string SecurityKeyOne { get; set; }
+        
+        [JsonPropertyName("security_key_two")]
         public string SecurityKeyTwo { get; set; }
     }
 }
