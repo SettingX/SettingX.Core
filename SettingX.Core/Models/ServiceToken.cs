d396881cabb6d5f748813cfd5a886f8f36abdb0b
Nika Gamkrelidze <nikagamkrelidze233@gmail.com>
2021-02-24 06:26:10 +0400

    ServiceToken fixed.

--- SettingX.Core/Models/ServiceToken.cs
+++ SettingX.Core/Models/ServiceToken.cs
@@ -2,7 +2,6 @@ namespace SettingX.Core.Models
 {
     public class ServiceToken
     {
-        public string Id { set; get; }
         public string Token { get; set; }
         public string SecurityKeyOne { get; set; }
         public string SecurityKeyTwo { get; set; }
